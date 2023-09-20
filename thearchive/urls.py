a9589659f404683ddaf8d4de6669ede2d4081013
Eric Frey <ericlfrey@gmail.com>
2023-09-20 14:47:04 -0500

    Added register and checkuser urls to the urls.py file.

--- thearchive/urls.py
+++ thearchive/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from thearchiveapi.views import register_user, check_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_user),
+    path('checkuser/', check_user),
 ]
