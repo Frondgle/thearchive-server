10c2d77470642f021844811a3e444a5b73de03a6
Angie Gonzalez <angiem.go@gmail.com>
2023-09-22 17:20:19 -0500

    tag URL updated

--- thearchiveapi/views/tag.py
+++ thearchiveapi/views/tag.py
@@ -1,5 +1,5 @@
 """View module for handling requests about Tags"""
-from django.http import HttpResponseServerError
+# from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
