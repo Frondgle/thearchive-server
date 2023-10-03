0528c361034cb6b3a72bb7ae1b9b556d733b9b2d
Angie Gonzalez <angiem.go@gmail.com>
2023-10-02 21:44:06 -0500

    trying to add tag widget to upload arts form

--- thearchiveapi/admin.py
+++ thearchiveapi/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-# Register your models here.
-from .models import Art
-from .models import Tag
+from .models import Art, Tag
 
+
+class ArtAdmin(admin.ModelAdmin):
+    """Class to customize the admin interface for the Art model. 
+      Filter_horizontal OR filter_vertical widget/layout
+      to add the tags field to the Art model admin page.
+  """
+    # Other admin options for the Art model
+    # ...
+    filter_horizontal = ('tags',)
+    # filter_vertical = ('tags',)
+
+# Register your models with the custom admin class
 admin.site.register(Art)
 admin.site.register(Tag)
