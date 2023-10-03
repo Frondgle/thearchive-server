0467a77368f4a04cf3701ce4f5f4ed359d8a6540
Angie Gonzalez <angiem.go@gmail.com>
2023-10-02 21:21:10 -0500

    note to not migrate fixtures with fake data--added Tag to Django admin

--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,7 @@ rm -rf ./thearchiveapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations thearchiveapi
 python3 manage.py migrate thearchiveapi
+## check fixtures
 python3 manage.py loaddata fans
 python3 manage.py loaddata tags
 python3 manage.py loaddata art
--- thearchiveapi/admin.py
+++ thearchiveapi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
 # Register your models here.
-
 from .models import Art
+from .models import Tag
 
 admin.site.register(Art)
+admin.site.register(Tag)
