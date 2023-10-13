dacdee8164a48ec577613ee06b9c18b9cad6156d
Angie Gonzalez <angiem.go@gmail.com>
2023-10-12 22:05:30 -0500

    per book-deleted scratchsql and made Django secret key

--- thearchive/settings.py
+++ thearchive/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-60&^u@4+r*0aairmq#u-s$gvvk!jp7^ip6kwvchz8f@0#&*=ec'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
