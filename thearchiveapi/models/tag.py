f8699733718c29b5a90dbac6b618316ebadd166c
Angie Gonzalez <angiem.go@gmail.com>
2024-02-05 19:02:43 -0600

    case insensitive model for tag

--- thearchiveapi/models/tag.py
+++ thearchiveapi/models/tag.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    category = models.CharField(max_length=255, unique=True)
+    category = models.CharField(max_length=255, unique=_ciTrue)
 
     def __str__(self):
         return self.category
