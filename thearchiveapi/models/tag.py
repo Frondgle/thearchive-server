bf694945f290713b5f222173ad80fd4a3eeb1026
Angie Gonzalez <angiem.go@gmail.com>
2024-02-05 19:11:04 -0600

    updated Django Unique and case insensitive string comparison

--- thearchiveapi/models/tag.py
+++ thearchiveapi/models/tag.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.db.models.functions import Lower
 
 
 class Tag(models.Model):
-    category = models.CharField(max_length=255, unique=_ciTrue)
+    category = models.CharField(max_length=255)
+
+    class Meta: 
+        constraints = [
+            models.UniqueConstraint(Lower('category'), name='unique_tag')
+        ]
 
     def __str__(self):
         return self.category
